a69605a5e6fcf299e315dbc2066239d3ee2d30a2
Ambre Gorias <ambre.gorias@gmail.com>
2022-05-02 13:40:09 -1000

    implemented persisting cart data with redux-persist

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
 import App from "./App";
 
 import { store } from "./store/store";
+import { persistor } from "./store/store";
 
 import "./index.scss";
 
@@ -13,9 +15,11 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <Router>
-        <App />
-      </Router>
+      <PersistGate loading={null} persistor={persistor}>
+        <Router>
+          <App />
+        </Router>
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
--- src/store/store.js
+++ src/store/store.js
@@ -2,9 +2,12 @@
 // For learning purposes we use redux core library but reduxjs/toolkit should used instead
 import { compose, createStore, applyMiddleware } from "redux";
 // import logger from "redux-logger";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
 import { rootReducer } from "./root-reducer";
 
+// Homemade logger
 const loggerMiddleware = (store) => (next) => (action) => {
   if (!action.type) {
     return next(action);
@@ -19,6 +22,16 @@ const loggerMiddleware = (store) => (next) => (action) => {
   console.log("next state: ", store.getState());
 };
 
+// Persist configuration
+const persistConfig = {
+  key: "root",
+  storage,
+  // user blacklisted because it is already coming from our auth listener => avoid conflict
+  blacklist: ["user", "categories"],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 // Middlewares declaration
 const middleWares = [loggerMiddleware];
 
@@ -26,4 +39,10 @@ const middleWares = [loggerMiddleware];
 // applyMiddleware: create a store enhancer
 const composedEnhancers = compose(applyMiddleware(...middleWares));
 
-export const store = createStore(rootReducer, undefined, composedEnhancers);
+export const store = createStore(
+  persistedReducer,
+  undefined,
+  composedEnhancers
+);
+
+export const persistor = persistStore(store);
