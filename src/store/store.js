4020b776caed9d33bb5448f6b0a395b69b2c3a7d
Ambre Gorias <ambre.gorias@gmail.com>
2022-05-02 18:20:58 -1000

    migrated categories fetching to redux-thunk

--- src/store/store.js
+++ src/store/store.js
@@ -1,43 +1,39 @@
 // State lives here // Actions received and dispatched here
 // For learning purposes we use redux core library but reduxjs/toolkit should used instead
 import { compose, createStore, applyMiddleware } from "redux";
-// import logger from "redux-logger";
+import logger from "redux-logger";
 import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
+import thunk from "redux-thunk";
 
 import { rootReducer } from "./root-reducer";
-
-// Homemade logger
-const loggerMiddleware = (store) => (next) => (action) => {
-  if (!action.type) {
-    return next(action);
-  }
-
-  console.log("type", action.type);
-  console.log("payload", action.payload);
-  console.log("currentState: ", store.getState());
-
-  next(action);
-
-  console.log("next state: ", store.getState());
-};
 
 // Persist configuration
 const persistConfig = {
   key: "root",
   storage,
   // user blacklisted because it is already coming from our auth listener => avoid conflict
-  blacklist: ["user", "categories"],
+  whitelist: ["cart"],
 };
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 // Middlewares declaration
-const middleWares = [loggerMiddleware];
+const middleWares = [
+  process.env.NODE_ENV !== "production" && logger,
+  thunk,
+].filter(Boolean); // .filter(Boolean) to get an empty array instead of a 'false' when NODE_ENV === 'production'
+
+// When in development we use the redux devtools compose, otherwise redux regular compose
+const composeEnhancer =
+  (process.env.NODE_ENV !== "production" &&
+    window &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+  compose;
 
 // compose: Pass multiple functions left to right
 // applyMiddleware: create a store enhancer
-const composedEnhancers = compose(applyMiddleware(...middleWares));
+const composedEnhancers = composeEnhancer(applyMiddleware(...middleWares));
 
 export const store = createStore(
   persistedReducer,
